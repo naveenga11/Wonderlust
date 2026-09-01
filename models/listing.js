ee8b08bfd94ead1fe79f5aceacb020fb87092c86
Naveen G A <naveenga06@gmail.com>
2026-09-01 23:23:05 +0530

    defined review schema

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const reviews = require("./reviews");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -26,6 +27,12 @@ const listingSchema = new Schema({
 ,
   location: String,
   country: String,
+  reviews:[
+    {
+      type:Schema.Types.ObjectId,
+      ref :"Review",
+    }
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
